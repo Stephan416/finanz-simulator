1eb6c0015baa35b01720b97892f7904c44d16693
Stephan416 <kontakt.strobel@gmail.com>
2026-04-26 11:23:50 +0200

    Modell_1_2_Eingabekasten_links_design.py

--- Modell_1_2_Eingabekasten_links_design.py
+++ Modell_1_2_Eingabekasten_links_design.py
@@ -301,7 +301,7 @@ with eingabe_spalte:
 
     # Freiheitsgrade der t-Verteilung.
     # Niedriger = mehr extreme Ausschläge.
-     marktrisiko = st.selectbox(
+    marktrisiko = st.selectbox(
         "Marktrisiko",
         options=[
             "Normale Märkte",
