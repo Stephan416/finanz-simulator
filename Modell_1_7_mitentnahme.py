99e305bcaf24a9a331ffed92f98e52bb0d3969f8
Stephan416 <kontakt.strobel@gmail.com>
2026-05-01 10:28:36 +0200

    Add files via upload

--- Modell_1_7_mitentnahme.py
+++ Modell_1_7_mitentnahme.py
@@ -275,16 +275,16 @@ with eingabe_spalte:
         entnahmequote_pro_jahr = monatliche_entnahme * 12 / startwert * 100
 
     if entnahmequote_pro_jahr <= 3.5:
-        st.success(
+        st.caption(
             f"✅ Entnahmequote: {entnahmequote_pro_jahr:.1f} % p.a. – eher konservativ"
         )
     elif entnahmequote_pro_jahr <= 5.0:
-        st.warning(
+        st.caption(
             f"⚠️ Entnahmequote: {entnahmequote_pro_jahr:.1f} % p.a. – ambitioniert"
         )
     else:
-        st.error(
-            f"🚨 Entnahmequote: {entnahmequote_pro_jahr:.1f} % p.a. – sehr hoch"
+        st.caption(
+        f"🚨 Entnahmequote: {entnahmequote_pro_jahr:.1f} % p.a. – sehr hoch"
         )
 
     
